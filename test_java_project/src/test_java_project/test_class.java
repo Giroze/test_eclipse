c59e25691710d573314fa6b9fb7dd6a2342d49f0
giroze <dktmvozk.gamil.com>
2019-03-27 10:48:31 +0900

    hotfix branch change a code

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -1,5 +1,8 @@
 package test_java_project;
 
 public class test_class {
-
+	public static void main(String[] args) {
+		System.out.println("hello world-! hotpix!");
+		System.out.println("i'm the hotfix~!");
+	}
 }
